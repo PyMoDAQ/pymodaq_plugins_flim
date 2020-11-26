2e7ae15374072e9c32e29d9d6dcfbfaa152cf2c5
Sébastien Weber <seba.weber@gmail.com>
2020-11-26 17:24:39 +0100

    added dependences

--- plugin_info.py
+++ plugin_info.py
@@ -1,5 +1,5 @@
 ## To modify by developper(s) of the plugin
-SHORT_PLUGIN_NAME = 'a_name'  #for instance daqmx
+SHORT_PLUGIN_NAME = 'flim'  #for instance daqmx
 package_url = 'https://github.com/CEMES-CNRS/pymodaq_plugins_template' #to modify
 description = 'Set of PyMoDAQ plugins for FLIM Measurements (Fluorescence Lifetime Imaging Microscopy) (using a ' \
               'conjugation of Time-Correlated Single Photon Counting (TSCPC) and xy stages)'
@@ -9,5 +9,5 @@ author_email = 'sebastien.weber@cemes.fr'
 
 #packages required for your plugin:
 packages_required = ['serial',
-                     'skimage',]
+                     'scikit-image',]
 ##
--- pymodaq_plugins_flim/daq_viewer_plugins/plugins_ND/daq_NDviewer_FLIM_TH260_PiezoConcept.py
+++ pymodaq_plugins_flim/daq_viewer_plugins/plugins_ND/daq_NDviewer_FLIM_TH260_PiezoConcept.py
@@ -18,8 +18,8 @@ from pymodaq.daq_utils.scanner import ScanParameters
 
 from pyqtgraph.parametertree import Parameter, ParameterTree
 import pyqtgraph.parametertree.parameterTypes as pTypes
-import pymodaq.daq_utils.custom_parameter_tree as custom_tree
-
+import pymodaq.daq_utils.parameter.pymodaq_ptypes
+from pymodaq.daq_utils.parameter.utils import iter_children
 from pymodaq_plugins_piezoconcept.hardware.piezoconcept.piezoconcept import PiezoConcept, Position, Time
 from pymodaq_plugins_picoquant.daq_viewer_plugins.plugins_1D.daq_1Dviewer_TH260 import DAQ_1DViewer_TH260, T3Reader
 import time
@@ -90,7 +90,7 @@ class DAQ_NDViewer_FLIM_TH260_PiezoConcept(DAQ_1DViewer_TH260):
 
     def commit_settings(self, param):
 
-        if param.name() not in custom_tree.iter_children(self.settings.child(('stage_settings'))):
+        if param.name() not in iter_children(self.settings.child(('stage_settings')), []):
             super().commit_settings(param)
 
         else:
@@ -104,7 +104,7 @@ class DAQ_NDViewer_FLIM_TH260_PiezoConcept(DAQ_1DViewer_TH260):
                 self.emit_status(ThreadCommand('show_scanner'))
                 param.setValue(False)
 
-            elif param.name() in custom_tree.iter_children(self.settings.child('stage_settings', 'move_at'), []):
+            elif param.name() in iter_children(self.settings.child('stage_settings', 'move_at'), []):
                 pos_x = self.settings.child('stage_settings', 'move_at', 'move_at_x').value()
                 pos_y = self.settings.child('stage_settings', 'move_at', 'move_at_y').value()
                 self.move_at_navigator(pos_x, pos_y)
